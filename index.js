b590903aacbdfd578cfdd78f79551593d53958e5
asumpta <sumgathoni@gmail.com>
2023-01-25 19:38:21 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -9,57 +9,182 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+// const allWagesFor = function () {
+//     const eligibleDates = this.timeInEvents.map(function (e) {
+//         return e.date
+//     })
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+//     const payable = eligibleDates.reduce(function (memo, d) {
+//         return memo + wagesEarnedOnDate.call(this, d)
+//     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
-}
+//     return payable
+// }
 
-// Create an employee record from an array of strings
-function createEmployeeRecord(employeeArray) {
-    const [firstName, familyName, title, payPerHour] = employeeArray;
-    return {
-      firstName,
-      familyName,
-      title,
-      payPerHour,
-      timeInEvents: [],
-      timeOutEvents: []
-    }
-  }
+// // Create an employee record from an array of strings
+// function createEmployeeRecord(employeeArray) {
+//     const [firstName, familyName, title, payPerHour] = employeeArray;
+//     return {
+//       firstName,
+//       familyName,
+//       title,
+//       payPerHour,
+//       timeInEvents: [],
+//       timeOutEvents: []
+//     }
+//   }
   
-  // Create an array of employee records from an array of arrays
-  function createEmployeeRecords(employeeArrays) {
-    return employeeArrays.map(employeeArray => createEmployeeRecord(employeeArray));
-  }
+//   // Create an array of employee records from an array of arrays
+//   function createEmployeeRecords(employeeArrays) {
+//     return employeeArrays.map(employeeArray => createEmployeeRecord(employeeArray));
+//   }
   
-  // Create a time in event
-  function createTimeInEvent(dateStamp) {
-    const [date, hour] = dateStamp.split(" ");
-    return {
-      type: "TimeIn",
-      hour,
-      date
-    };
-  }
+//   // Create a time in event
+//   function createTimeInEvent(dateStamp) {
+//     const [date, hour] = dateStamp.split(" ");
+//     return {
+//       type: "TimeIn",
+//       hour,
+//       date
+//     };
+//   }
   
-  // Create a time out event
-  function createTimeOutEvent(dateStamp) {
-    const [date, hour] = dateStamp.split(" ");
-    return {
-      type: "TimeOut",
-      hour,
-      date
-    };
-  }
+//   // Create a time out event
+//   function createTimeOutEvent(dateStamp) {
+//     const [date, hour] = dateStamp.split(" ");
+//     return {
+//       type: "TimeOut",
+//       hour,
+//       date
+//     };
+//   }
   
+//****/
+
 //   // Calculate the hours worked on a specific date
 //   function hoursWorkedOnDate(employeeRecord, date) {
 //     const timeInEvents = employeeRecord.timeInEvents.filter(event => event
 
+
+
+
+// TASK ONE
+// let testEmployee;
+// testEmployee = ["Gray", "Worm", "Security", 1];
+// let result;
+// result = createEmployeeRecord(testEmployee);
+// console.log(result);
+
+function createEmployeeRecord(fourElementArray) {
+  let [firstName, familyName, title, payPerHour] = fourElementArray;
+  let objectBuilt = {
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return objectBuilt;
+}
+
+// TASK TWO
+// let employeeRecords;
+// let twoRows = [
+//   ["moe", "sizlak", "barkeep", 2],
+//   ["bartholomew", "simpson", "scamp", 3],
+// ];
+// let answer;
+// answer = createEmployeeRecords(twoRows);
+// console.log(answer);
+
+function createEmployeeRecords(arrayOfArrays) {
+  return arrayOfArrays.map((member) => {
+    return createEmployeeRecord(member);
+  });
+}
+
+// TASK THREE
+// let answer;
+// answer = createTimeInEvent(
+//   {
+//     firstName: "Gray",
+//     familyName: "Worm",
+//     title: "Security",
+//     payPerHour: 1,
+//     timeInEvents: [],
+//     timeOutEvents: [],
+//   },
+//   "2014-02-28 1400"
+// );
+// console.log(answer);
+
+function createTimeInEvent(dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  });
+  return this;
+}
+
+// TASK FOUR
+// copy and tweak the code for createTimeInEvent() function
+
+function createTimeOutEvent(dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+  return this;
+}
+
+// TASK FIVE
+// let cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 1000]);
+// let timeIn = createTimeInEvent(cRecord, "0044-03-15 0900");
+// let timeOut = createTimeOutEvent(cRecord, "0044-03-15 1100");
+// hoursWorkedOnDate(cRecord, "0044-03-15");
+
+function hoursWorkedOnDate(dateArg) {
+  let inEvents = this.timeInEvents.find((e) => e.date === dateArg);
+  let outEvents = this.timeOutEvents.find((e) => e.date === dateArg);
+  return (outEvents.hour - inEvents.hour) / 100;
+}
+
+// TASK SIX
+// draw inspiration from TASK FIVE
+
+function wagesEarnedOnDate(dateArg) {
+  let raw = hoursWorkedOnDate.call(this, dateArg) * this.payPerHour;
+  return parseFloat(raw.toString());
+}
+
+// TASK SEVEN
+
+const allWagesFor = function () {
+  let avaiableDates = this.timeInEvents.map((obj) => obj.date);
+  let payable = avaiableDates.reduce(
+    function (accumulator, value) {
+      return accumulator + wagesEarnedOnDate.call(this, value);
+    }.bind(this),
+    0
+  );
+  return payable;
+};
+
+// TASK EIGHT
+
+function findEmployeeByFirstName(collection, firstNameString) {
+  return collection.find((obj) => obj.firstName === firstNameString);
+}
+
+// TASK NINE
+
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce((accumulator, value) => {
+    return accumulator + allWagesFor.call(value);
+  }, 0);
+};
